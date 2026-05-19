0f8cddd47ef52afa8cf991ac487e02d14a9f014c
Guilherme <153320263+Guilhermepycharm@users.noreply.github.com>
2026-05-19 12:35:38 -0300

    Atualizar o shell.nix

--- config/shell.nix
+++ config/shell.nix
@@ -19,7 +19,6 @@ pkgs.mkShell {
     export QT_QPA_PLATFORM=wayland
     export QT_PLUGIN_PATH="${pkgs.qt6.qtspeech}/lib/qt-6/plugins:${pkgs.qt6.qtbase}/lib/qt-6/plugins"
     export LD_LIBRARY_PATH="${pkgs.qt6.qtbase}/lib:${pkgs.espeak-ng}/lib:${pkgs.pipewire}/lib:$LD_LIBRARY_PATH"
-    echo "✦ Ambiente Aurudinha atualizado com Voz e Áudio! ✦"
     echo "Para compilar: cmake . && make"
     echo "Para rodar: ./aurudinha_game"
   '';
